bf0593b39cf7d19a2721397f81f8dddb1e91c60c
Broc Gailit <brocgailit@gmail.com>
2016-10-05 20:19:28 -0700

    Initial Next Page directive implementation

--- src/app/shared/next-page.directive.ts
+++ src/app/shared/next-page.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer, AfterViewInit } from '@angular/core';
 
 @Directive({
-  selector: '[appNextPage]'
+  selector: '[nextPage]'
 })
-export class NextPageDirective {
+export class NextPageDirective implements AfterViewInit {
 
-  constructor() { }
+  @Input('nextPage') nextPage: string;
+
+  constructor(private el: ElementRef, private renderer: Renderer) { }
+
+  ngAfterViewInit() {
+    let height = 50;
+    let margin = 50;
+    let bottom = this.el.nativeElement.getBoundingClientRect().bottom - height - margin;
+    let button = this.renderer.createElement(this.el.nativeElement, 'a');
+    button.innerHTML = '&darr;';
+    this.renderer.setElementClass(button, 'next-page', true);
+    this.renderer.setElementAttribute(button, 'href', `#${this.nextPage}`);
+    this.renderer.setElementStyle(button, 'top', bottom + 'px');
+    this.renderer.setElementStyle(button, 'fontSize', `${height}px`);
+    this.renderer.setElementStyle(button, 'lineHeight', `${height}px`);
+  }
 
 }
