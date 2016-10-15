c2ad12cc39742b3b5bc6a170c7f986f856828e37
Broc Gailit <brocgailit@gmail.com>
2016-10-15 12:35:42 -0700

    Add basic enter animation on hero

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,10 +1,30 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit,
+  trigger, state, style, transition, animate } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'hc-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
+  animations: [
+    trigger('flyIn', [
+      state('in', style({
+        transform: 'translateY(0)',
+        opacity: 1
+      })),
+      transition(':enter', [
+        style({
+          transform: 'translateY(25%)',
+          opacity: 0
+        }),
+        animate('1.5s cubic-bezier(0.23, 1, 0.32, 1)')
+      ])
+    ]),
+    trigger('fadeIn', [
+      state('in', style({opacity: 1})),
+      transition(':enter', [style({opacity: 0}), animate('1s 750ms ease-out')])
+    ])
+  ]
 })
 export class HeroComponent implements OnInit {
 
